e8108ad65a093bbe743dd4f0b083c818d53f3ce4
dlabaja <dlabaja.jan@seznam.cz>
2024-12-16 15:42:23 +0100

    pogrs

--- src/app/i18n.ts
+++ src/app/i18n.ts
@@ -1,31 +1,14 @@
-import { createInstance } from "i18next";
-import { initReactI18next } from "react-i18next/initReactI18next";
-import resourcesToBackend from "i18next-resources-to-backend";
-import i18nConfig from "../i18nConfig"
+import {Translator} from "@/data/translator";
 
 export default async function initTranslations(
     locale: string,
     // eslint-disable-next-line
     i18n: any
 ) {
-    const i18nInstance = i18n || createInstance();
-
-    i18nInstance
-        .use(initReactI18next)
-        .use(resourcesToBackend(
-            (language: string) =>
-                import((`../lang/${language}.json`))
-        ));
-
-    await i18nInstance.init({
-        lng: locale,
-        fallbackLng: i18nConfig.defaultLocale,
-        supportedLngs: i18nConfig.locales,
-        preload: i18nConfig.locales
-    });
+    const instance = await new Translator().init(locale, i18n)
 
     return {
-        i18n: i18nInstance,
-        t: i18nInstance.t
+        i18n: instance,
+        t: instance.t
     };
 }
